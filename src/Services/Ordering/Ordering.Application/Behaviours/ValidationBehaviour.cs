1c286af7054cd5497a08fe70a29fe054743f75bb
yansleptsov4 <yansleptsov4@gmail.com>
2023-06-09 22:23:35 +0600

    ordering api dockerized and implemented

--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -10,6 +10,7 @@ using ValidationException = Ordering.Application.Exceptions.ValidationException;
 namespace Ordering.Application.Behaviours
 {
     public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : MediatR.IRequest<TResponse>
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
 
@@ -18,14 +19,16 @@ namespace Ordering.Application.Behaviours
             _validators = validators ?? throw new ArgumentNullException(nameof(validators));
         }
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
-            RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, 
+            RequestHandlerDelegate<TResponse> next, 
+            CancellationToken cancellationToken)
         {
             if (_validators.Any())
             {
                 var context = new ValidationContext<TRequest>(request);
 
-                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var validationResults = await Task.WhenAll(_validators
+                    .Select(v => v.ValidateAsync(context, cancellationToken)));
                 var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
 
                 if (failures.Count != 0)
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {
+                    UserName = "swn",
+                    FirstName = "Mehmet", 
+                    LastName = "Ozkaya", 
+                    EmailAddress = "ezozkme@gmail.com", 
+                    AddressLine = "Bahcelievler", 
+                    Country = "Turkey", 
+                    TotalPrice = 350, 
+                    CVV = "4568",
+                    CardName = "xxxx",
+                    CardNumber = "123",
+                    Expiration = "June 2025",
+                    PaymentMethod = 1,
+                    State = "ok",
+                    ZipCode = "2233",
+                    /*CreatedBy = "swn",
+                    CreatedDate = DateTime.Now,
+                    LastModifiedBy = "swn",
+                    LastModifiedDate = DateTime.Now*/
+                }
+            };
+        }
+    }
+}
